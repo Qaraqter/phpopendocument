6b73c49c3a3d8625fa13381c6b5a6b55f4130573
Bart Huttinga <bart@qaraqter.nl>
2014-04-28 11:22:46 +0200

    fix loader

--- lib/OpenDocument/Twig/OpenDocumentLoader.php
+++ lib/OpenDocument/Twig/OpenDocumentLoader.php
@@ -11,6 +11,7 @@ class OpenDocumentLoader extends \Twig_Loader_Filesystem
         $source = parent::getSource($name);
 
         $this->fixStyledTags($source);
+        $this->fixEmptyParagraphs($source);
         $this->fixTableRowForLoop($source);
 
         return $source;
@@ -52,6 +53,27 @@ class OpenDocumentLoader extends \Twig_Loader_Filesystem
     }
 
     /**
+     * Fix empty paragraphs.
+     *
+     * Strips paragraph tags from paragraphs that only contain Twig-tags and do
+     * not output anything.
+     */
+    private function fixEmptyParagraphs(&$source)
+    {
+        $pattern = '/\<text:p[^>]*?\>({%.*?%})<\/text:p\>/';
+
+        $callback = function(array $matches) {
+            // no replacement if the tag contains Twig output tags
+            if (preg_match('/({{|}})/', $matches[1], $newMatches)) {
+                return $matches[0];
+            }
+            return $matches[1];
+        };
+
+        $source = preg_replace_callback($pattern, $callback, $source);
+    }
+
+    /**
      * Fix for-loops in tables.
      *
      * If a for-loop applies to the whole row, the start-tag should be placed
@@ -59,27 +81,12 @@ class OpenDocumentLoader extends \Twig_Loader_Filesystem
      */
     private function fixTableRowForLoop(&$source)
     {
-        $document = new \DOMDocument();
-        $document->loadXML($source);
+        $pattern = '#(<table:table-row[^>]*?><table:table-cell[^>]*?><text:p[^>]*?>)({% for .* %})(.*)({% endfor %})(<\/text:p><\/table:table-cell><\/table:table-row>)#';
 
-        $xpath = new \DOMXpath($document);
+        $callback = function(array $matches) {
+            return $matches[2] . $matches[1] . $matches[3] . $matches[5] . $matches[4];
+        };
 
-        $tables = $document->getElementsByTagName('table');
-        foreach ($tables as $table) {
-            $tableRows = $xpath->query('table:table-row', $table);
-            foreach ($tableRows as $tableRow) {
-                if (preg_match('/^({%\s*for.*%}).*({%\s*endfor\s*%})$/', $tableRow->nodeValue)) {
-                    $tableXml = $document->saveXML($table);
-                    $pattern = '/^(.*)(<table:table-row.*>)({%\s*for.*%})(.*)({%\s*endfor\s*%})(.*<\/table:table-row>)(.*)$/';
-                    $tableXml = preg_replace($pattern, '$1$3$2$4$6$5$7', $tableXml);
-
-                    $fragment = $document->createDocumentFragment();
-                    @$fragment->appendXML($tableXml);
-                    $xpath->query('..', $table)->item(0)->replaceChild($fragment, $table);
-                }
-            }
-        }
-
-        $source = $document->saveXML();
+        $source = preg_replace_callback($pattern, $callback, $source);
     }
 }
